50416f5140188076640831f4ea6281b7892e3cc2
Mahmoud Farghali <101347618@georgebrown.ca>
2023-03-18 03:55:25 -0400

    herkou deployed

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
+
+app.get('/path/to/file.pdf', function(req, res) {
+    res.setHeader('Content-Disposition', 'attachment; filename=resume.pdf');
+    res.sendFile('/path/to/resume.pdf');
+  });
+
 const port = process.env.PORT || 3000;
 app.listen(port, function() {
   console.log('App listening on port ' + port);
